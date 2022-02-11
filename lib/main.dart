d6d4aba00af4a2b03030bdd5a6a45bce608e859d
David. J <codewithjalloh@gmail.com>
2022-02-11 13:03:54 +0000

    part one complete

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,12 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Welcome! ", style: Theme.of(context).textTheme.headline2),
+        child: Column(
+          children: [
+            Text("Welcome! ", style: Theme.of(context).textTheme.headline2),
+            Text("Waht would you like to do today Mr"),
+          ],
+        )
       ),
     );
   }
